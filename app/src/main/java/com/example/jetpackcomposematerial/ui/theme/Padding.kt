327528c7586e37f3bebfd1ea9ba4d83fc0bd723d
elye <elye.project@gmail.com>
2021-03-27 19:08:12 +1100

    Initial commit

---
+++ app/src/main/java/com/example/jetpackcomposematerial/ui/theme/Padding.kt
@@ -0,0 +1,16 @@
+package com.example.jetpackcomposematerial.ui.theme
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+
+@Immutable
+data class Paddings(
+    val defaultPadding: Dp = 16.dp,
+    val smallPadding: Dp = 8.dp,
+    val largePadding: Dp = 24.dp
+)
+
+internal val LocalPaddings = staticCompositionLocalOf { Paddings() }
---
+++ app/src/main/java/com/example/jetpackcomposematerial/ui/theme/Theme.kt
@@ -0,0 +1,89 @@
+package com.example.jetpackcomposematerial.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.unit.dp
+
+private val DarkColorPalette = darkColors(
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
+
+private val LightColorPalette = lightColors(
+    primary = Purple500,
+    primaryVariant = Purple700,
+    secondary = Teal200
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun JetpackComposeMaterialTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable() () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+    CompositionLocalProvider(
+        LocalPaddings provides Paddings(),
+        LocalElevations provides Elevations(card = 8.dp)
+    ) {
+        MaterialTheme(
+            colors = colors,
+            typography = Typography,
+            shapes = Shapes,
+            content = content
+        )
+    }
+}
+
+object JetpackComposeMaterialTheme {
+    /**
+     * Proxy to [MaterialTheme]
+     */
+    val colors: Colors
+        @Composable
+        get() = MaterialTheme.colors
+
+    /**
+     * Proxy to [MaterialTheme]
+     */
+    val typography: Typography
+        @Composable
+        get() = MaterialTheme.typography
+
+    /**
+     * Proxy to [MaterialTheme]
+     */
+    val shapes: Shapes
+        @Composable
+        get() = MaterialTheme.shapes
+
+    /**
+     * Retrieves the current [Paddings].
+     */
+    val paddings: Paddings
+        @Composable
+        get() = LocalPaddings.current
+
+    /**
+     * Retrieves the current [Paddings].
+     */
+    val elevations: Elevations
+        @Composable
+        get() = LocalElevations.current
+
+}
